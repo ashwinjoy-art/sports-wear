f8e8f5baeabc0d81667d637f931d4149c0276f7c
ashwinjoy-art <ashwinjoy530@gmail.com>
2024-09-19 11:44:50 +0530

    File Updates

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import { ShopContext } from '../contexts/ShopContext'
 import Title from './Title';
 import ProductItem from './ProductItem';
 
@@ -14,15 +14,15 @@ const LatestCollection = () => {
 
   return (
     <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
+        <div className='py-8 text-3xl text-center'>
             <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
-            <p className='w-3/4 m-auto text-xs sm:text-base text-gray-600'>
+            <p className='w-3/4 m-auto text-xs text-gray-600 sm:text-base'>
                 Nike Sportswear Phoenix Fleece Unisexual High-Waisted Loose French Terry Shorts
             </p>
         </div>
 
         {/*Rendering ProductS*/}
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6'>
             {
                 latestProducts.map((item,index)=>(
                     <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import { ShopContext } from '../contexts/ShopContext'
 import {Link} from 'react-router-dom'
 
 const ProductItem = ({id,image,name,price}) => {
@@ -9,7 +9,7 @@ const ProductItem = ({id,image,name,price}) => {
   return (
     <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
         <div className='overflow-hidden'>
-            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt=""/>
+            <img className='transition ease-in-out hover:scale-110' src={image[0]} alt=""/>
         </div>
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>{currency}{price}</p>

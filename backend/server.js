7d13f1203f76355cf05c465357421171b9f2f58f
ashwinjoy-art <ashwinjoy530@gmail.com>
2025-02-07 14:40:41 +0530

    vercel update

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,18 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(
+    cors({
+      origin: 'https://sports-wear-frontend.vercel.app', // Allow only frontend
+      methods: 'GET, POST, PUT, DELETE, OPTIONS',
+      allowedHeaders: 'Content-Type, Authorization',
+      credentials: true,
+    })
+  );
+  
+  // Handle Preflight Requests
+  app.options('*', cors());
+  
 
 // API endpoint
 app.use('/api/user',userRouter)
